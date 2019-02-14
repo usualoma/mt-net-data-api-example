7773de2ff81739f3d9b911121faab113ea4059ee
Taku AMANO <taku@taaas.jp>
2019-02-14 14:05:56 +0900

    Render component after domready.

--- mt-entries/src/index.js
+++ mt-entries/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import domready from "domready";
 
-const elm = document.getElementById("mt-entries");
-ReactDOM.render(
-  <App
-    url={elm.dataset.url}
-    perPage={parseInt(elm.dataset.perPage)}
-    limit={parseInt(elm.dataset.limit)}
-  />,
-  elm
-);
+domready(() => {
+  const elm = document.getElementById("mt-entries");
+  ReactDOM.render(
+    <App
+      url={elm.dataset.url}
+      perPage={parseInt(elm.dataset.perPage)}
+      limit={parseInt(elm.dataset.limit)}
+    />,
+    elm
+  );
+});
